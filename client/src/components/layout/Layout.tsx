aec906bfb38858f144c5905245ec0f4a5ca3fc3b
Caleb Haldane <caldane@gmail.com>
2026-07-28 18:33:05 -0500

    ui: fix top nav for mobile pages

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,21 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faHouse } from "@fortawesome/free-solid-svg-icons";
 import { Outlet, useNavigate } from "react-router-dom";
+import style from "./layout.module.css";
 
 const Layout = () => {
     const navigate = useNavigate();
 
     return (
         <>
-            <nav>
-                <button onClick={() => navigate(-1)}>
+            <nav className={style.topNav}>
+                <button className={style.navButton} onClick={() => navigate(-1)}>
                     <FontAwesomeIcon icon={faArrowLeft} /> Back
                 </button>
-                <button onClick={() => navigate("/")}>
+                <button className={style.navButton} onClick={() => navigate("/")}>
                     <FontAwesomeIcon icon={faHouse} /> Home
                 </button>
             </nav>
-            <Outlet />
+            <main className={style.content}>
+                <Outlet />
+            </main>
         </>
     );
 };
